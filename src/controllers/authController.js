0d6afede8f31f7be6045b445e57c67aa52d34358
Labib Abdullah <labibabdullahhsan@gmail.com>
2026-02-22 23:22:36 +0700

    refactor(auth): remove unused verifyToken function from authController

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,27 +1,5 @@
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
-
-const verifyToken = (req, res, next) => {
-    // Mengambil token dari header Authorization (Format: Bearer <token>)
-    const authHeader = req.headers['authorization'];
-    const token = authHeader && authHeader.split(' ')[1];
-
-    if (!token) {
-        return res.status(403).json({ status: 'error', message: 'Token tidak disediakan!' });
-    }
-
-    // Memverifikasi token
-    jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
-        if (err) {
-            return res.status(401).json({ status: 'error', message: 'Token tidak valid atau kadaluarsa!' });
-        }
-
-        // Menyimpan data user dari token ke dalam request untuk digunakan di controller
-        req.user = decoded;
-        next();
-    });
-};
-
 const users = [];
 
 const register = async (req, res) => {
